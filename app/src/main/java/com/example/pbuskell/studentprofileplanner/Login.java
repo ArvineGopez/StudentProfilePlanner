d947b7f5802068b6fbd9654b4676c406d6367ca6
JaredC-12 <jaredcastillo550@gmail.com>
2016-06-15 15:26:31 -0500

    June 15 , Final Project studentProfile Planner Arvine Gopez

--- app/src/main/java/com/example/pbuskell/studentprofileplanner/Login.java
+++ app/src/main/java/com/example/pbuskell/studentprofileplanner/Login.java
@@ -1,7 +1,9 @@
 package com.example.pbuskell.studentprofileplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,7 +11,10 @@ import android.widget.TextView;
 public class Login extends AppCompatActivity {
     EditText edtTxtUsername;
     EditText edtTxtPassword;
-    Button btnLogin;
+    Button btnLoginRed;
+    TextView txtVwErrorRed;
+    String userName;
+    String password;
 
 
     @Override
@@ -17,7 +22,42 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edtTxtUsername = (EditText) findViewById(R.id.edtTxtUsername);
-        edtTxtPassword = (EditText) findViewById(R.id.edtTxtPassword);
+        edtTxtUsername = (EditText) findViewById(R.id.edtxtUsername);
+        edtTxtPassword = (EditText) findViewById(R.id.edtxtPassword);
+        btnLoginRed=(Button)findViewById(R.id.btnLoginRed);
+        txtVwErrorRed=(TextView)findViewById(R.id.txtVwErrorRed);
+        userName = getIntent().getStringExtra("USERNAME");
+        password = getIntent().getStringExtra("PASSWORD");
+
+
     }
+
+
+    public void insertLogin (View vw) {
+        Intent studentPlanner = new Intent(this, studentPlanner.class);
+
+        if(userName.equals("USERNAME") && password.equals("PASSWORD")){
+            startActivityForResult(studentPlanner, 0);
+
+
+        }else{
+            if (edtTxtUsername.getText().length() == 0 || edtTxtPassword.getText().length() == 0) {
+                txtVwErrorRed.setText("YOU MUST FILL IN ALL THE BLANKS IN ORDER TO LOGIN!");
+
+            } else {
+                txtVwErrorRed.setText("Your username or password is incorrect!");
+
+            }
+
+        }
+
+
+        /** If user doesnt put anything on the blanks
+        * it will say that you need to fill everything out
+        * in order to login
+        */
+
+    }
+
+
 }
--- app/src/main/java/com/example/pbuskell/studentprofileplanner/SignUp.java
+++ app/src/main/java/com/example/pbuskell/studentprofileplanner/SignUp.java
@@ -4,11 +4,10 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,7 +16,6 @@ public class SignUp extends AppCompatActivity {
     private EditText edtTxtEmail;
     private EditText edtTxtPassword;
     private TextView txtvwError;
-    private Button btnLogIn;
     private TextView txtvwSuccessSignUp;
 
 
@@ -27,64 +25,78 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         edtTxtUsername = (EditText) findViewById(R.id.edtTxtUsername);
         edtTxtEmail = (EditText) findViewById(R.id.edtTxtEmail);
-        edtTxtPassword = (EditText) findViewById(R.id.edtTxtPassword);
+        edtTxtPassword = (EditText) findViewById(R.id.edtxtPassword);
         txtvwError = (TextView) findViewById(R.id.txtvwError);
-        btnLogIn = (Button) findViewById(R.id.btnLogIn);
 
     }
 
-    public void insertData(View vw) {
+    public void gotoLogIn (View vw){
+
+        if (edtTxtUsername.getText().length() == 0 || edtTxtEmail.getText().length() == 0
+                || edtTxtPassword.getText().length() == 0) {
+
+            txtvwError.setText("YOU MUST FILL IN ALL THE INFORMATION!!!");
+
+        } else {
+            new studentPlannerTask().execute();
+
+        }
+    }
+
+    public void insertData() {
+        Intent SignUp = new Intent(this, Login.class);
         String studentUserName;
         String studentEmail;
         String studentPassword;
-        SQLiteDatabase db;
+        SQLiteDatabase db = null;
         ContentValues studentValues = new ContentValues();
         StudentDataBase studentDataBase = new StudentDataBase(this, null, null, 0);
 
-        try {
+        studentEmail=edtTxtEmail.getText().toString();
+        studentUserName=edtTxtUsername.getText().toString();
+        studentPassword=edtTxtPassword.getText().toString();
+       // sends the information to the login activity
+        SignUp.putExtra("USERNAME",studentUserName);
+        SignUp.putExtra("PASSWORD",studentPassword);
+
+        try{
             db = studentDataBase.getWritableDatabase();
+            studentValues.put("USERNAME", studentUserName);
+            studentValues.put("EMAIL", studentEmail);
+            studentValues.put("PASSWORD", studentPassword);
+            db.close();
 
-            if (edtTxtUsername.getText().length() == 0 || edtTxtEmail.getText().length() == 0
-                    || edtTxtPassword.getText().length() == 0) {
+        }catch(SQLiteException e){
 
-                txtvwError.setText("YOU MUST FILL IN ALL THE INFORMATION!!!");
-
-            } else {
-
-                studentEmail=edtTxtEmail.getText().toString();
-                studentUserName=edtTxtUsername.getText().toString();
-                studentPassword=edtTxtPassword.getText().toString();
-                studentValues.put("USERNAME", studentUserName);
-                studentValues.put("EMAIL", studentEmail);
-                studentValues.put("PASSWORD", studentPassword);
+        }
                 /**
                  * 1.Set each variable equal to the values from the EditTexts
                  * 2. put each value into the ContentValues variable
                  * 3. Call the StudentDataBase insertElement method
                  * 4.Display that the element has been added successfully
                  */
-                txtvwError.setText("YOU HAVE SUCCESSFULLY SIGNED UP!!");
 
 
 
-            }
-            db.close();
-        } catch (SQLiteException e) {
-            txtvwError.setText("The database was not found");
+        startActivityForResult(SignUp,0);
+
+    }
+    private class studentPlannerTask extends AsyncTask<Void,Void,Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            insertData();
+            return null;
         }
-
-        btnLogIn.setVisibility(View.VISIBLE);
-
     }
 
-    public void gotoLogIn (View vw){
-        Intent SignUp = new Intent(this, Login.class);
-        startActivity(SignUp);
-    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         edtTxtEmail.setText("");
         edtTxtUsername.setText("");
         edtTxtPassword.setText("");
 
     }
+
 }
